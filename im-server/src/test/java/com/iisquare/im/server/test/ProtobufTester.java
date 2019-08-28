fe0b6be9549f4168b48d0d80e16a9f0c0016c90a
iisquare <iisquare@163.com>
2019-08-28 20:06:56 +0800

    issue test

--- im-server/src/test/java/com/iisquare/im/server/test/ProtobufTester.java
+++ im-server/src/test/java/com/iisquare/im/server/test/ProtobufTester.java
@@ -12,27 +12,33 @@ import java.util.Date;
 public class ProtobufTester {
 
     @Test
+    /**
+     * @see(https://github.com/protocolbuffers/protobuf/issues/6581)
+     */
     public void serializeTest() throws InvalidProtocolBufferException {
         IMUser.Contact.Row.Builder builder = IMUser.Contact.Row.newBuilder();
-        builder.setUserId("xxx").setDirection("send").setMessageId("hhhhhh");
-        builder.setContent("fasfsaf");
-        long time = new Date().getTime();
+        long time = new Date().getTime(); // random
         time = 1566988103970L; // ok
         time = 1566988066637L; // bad
-        System.out.println(time);
+        System.out.println("time:" + time);
         builder.setTime(time);
         ByteString b1 = builder.build().toByteString();
-        System.out.println(Arrays.toString(b1.toByteArray()));
-        String data = Base64.getEncoder().encodeToString(b1.toByteArray());
-        System.out.println(data);
-        byte[] b2 = Base64.getDecoder().decode(data);
-        System.out.println(Arrays.toString(b2));
-        String str = new String(b1.toByteArray());
-        System.out.println(str);
-        System.out.println(b1.toStringUtf8());
-        System.out.println(Arrays.toString(str.getBytes()));
-        IMUser.Contact.Row row = IMUser.Contact.Row.parseFrom(b2);
+        System.out.println("origin:" + Arrays.toString(b1.toByteArray()));
+        String encode = Base64.getEncoder().encodeToString(b1.toByteArray());
+        System.out.println("base64 encode:" + encode);
+        byte[] decode = Base64.getDecoder().decode(encode);
+        System.out.println("base64 decode:" + Arrays.toString(decode));
+        IMUser.Contact.Row row = IMUser.Contact.Row.parseFrom(decode);
+        System.out.println("-------ROW BEGIN-------");
         System.out.println(row);
+        System.out.println("-------ROW END-------");
+        String data = b1.toStringUtf8(); // same with new String(b1.toByteArray())
+        System.out.println("b1 data:" + data);
+        ByteString b2 = ByteString.copyFromUtf8(data);
+        System.out.println("b2 data:" + b2.toStringUtf8());
+        byte[] bytes = b2.toByteArray(); // same with data.getBytes()
+        System.out.println("bytes:" + Arrays.toString(bytes));
+        IMUser.Contact.Row.parseFrom(b2); // bad time will throw int64 exception
     }
 
 }
