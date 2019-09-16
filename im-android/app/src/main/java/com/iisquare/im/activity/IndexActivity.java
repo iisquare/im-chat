6daa09b690767e66a6b93cae7462a2410428c00e
iisquare <iisquare@163.com>
2019-09-16 17:44:24 +0800

    android tab

--- im-android/app/src/main/java/com/iisquare/im/activity/IndexActivity.java
+++ im-android/app/src/main/java/com/iisquare/im/activity/IndexActivity.java
@@ -26,23 +26,21 @@ public class IndexActivity extends ActivityBase {
         Map<String, Object> item = new HashMap<>();
         item.put("image", R.mipmap.ic_launcher_round);
         item.put("active", R.mipmap.ic_launcher);
-        item.put("text", "联系人");
         item.put("fragment", new ContactFragment());
-        tabs.put("contact", item);
+        tabs.put("联系人", item);
         item = new HashMap<>();
         item.put("image", R.mipmap.ic_launcher_round);
         item.put("active", R.mipmap.ic_launcher);
-        item.put("text", "我");
         item.put("fragment", new SettingFragment());
-        tabs.put("setting", item);
+        tabs.put("我", item);
     }
 
-    private View tabView(Map<String, Object> item) {
+    private View tabView(String key, Map<String, Object> item) {
         View view = LayoutInflater.from(this).inflate(R.layout.index_tab_item, null);
         ImageView tabIcon = view.findViewById(R.id.tabImage);
         tabIcon.setImageResource((Integer) item.get("image"));
         TextView tabText = view.findViewById(R.id.tabText);
-        tabText.setText(item.get("text").toString());
+        tabText.setText(key);
         return view;
     }
 
@@ -50,16 +48,30 @@ public class IndexActivity extends ActivityBase {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
-        GridLayout layout = findViewById(R.id.tab);
+        final GridLayout layout = findViewById(R.id.tab);
         layout.setColumnCount(tabs.size());
         for (Map.Entry<String, Map<String, Object>> entry : tabs.entrySet()) {
-            Map<String, Object> item = entry.getValue();
+            final Map<String, Object> item = entry.getValue();
             GridLayout.Spec rowSpan = GridLayout.spec(GridLayout.UNDEFINED, 1f);
             GridLayout.Spec colSpan = GridLayout.spec(GridLayout.UNDEFINED, 1f);
             GridLayout.LayoutParams params = new GridLayout.LayoutParams(rowSpan, colSpan);
-            layout.addView(tabView(item), params);
+            final View view = tabView(entry.getKey(), item);
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int count = layout.getChildCount();
+                    for (int i = 0; i < count; i++) {
+                        View child = layout.getChildAt(i);
+                        ImageView tabIcon = child.findViewById(R.id.tabImage);
+                        TextView tabText = child.findViewById(R.id.tabText);
+                        tabIcon.setImageResource((Integer) tabs.get(tabText.getText()).get(v.equals(child) ? "active" : "image"));
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, (Fragment) item.get("fragment")).commit();
+                }
+            });
+            layout.addView(view, params);
         }
-        getSupportFragmentManager().beginTransaction().replace(R.id.container, (Fragment) tabs.get("contact").get("fragment")).commit();
+        layout.getChildAt(0).performClick();
     }
 
 }
