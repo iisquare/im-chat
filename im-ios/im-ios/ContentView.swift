eadf909bb7754e6bafd37ffb986eb98d10aae96e
iisquare <iisquare@163.com>
2019-09-27 11:16:20 +0800

    swift

---
+++ im-ios/im-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  im-ios
+//
+//  Created by Ouyang on 2019/9/26.
+//  Copyright © 2019 IISquare. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
